c72c1e33be66fe68842926d453e92bd527576f36
spogulakonda <spogulakonda@digisprint.com>
2021-06-11 18:00:44 +0530

    Site Values added to configure

---
+++ src/main/java/com/liverpool/configuration/constants/ResourceBundleConstants.java
@@ -0,0 +1,9 @@
+package com.liverpool.configuration.constants;
+
+public interface ResourceBundleConstants {
+	
+	String ID ="_id";
+	String VALUE = "value";
+	String SITE_VALUES = "siteValues";
+
+}
--- src/main/java/com/liverpool/configuration/service/impl/ResourceBundleServiceImpl.java
+++ src/main/java/com/liverpool/configuration/service/impl/ResourceBundleServiceImpl.java
@@ -3,11 +3,13 @@ package com.liverpool.configuration.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.bson.Document;
 import org.springframework.stereotype.Service;
 
 import com.liverpool.configuration.beans.ResourceBundle;
+import com.liverpool.configuration.constants.ResourceBundleConstants;
 import com.liverpool.configuration.repository.ResourceBundleRepository;
 import com.liverpool.configuration.service.ResourceBundleService;
 import com.mongodb.client.model.ReplaceOptions;
@@ -19,6 +21,9 @@ public class ResourceBundleServiceImpl implements ResourceBundleService{
 	
 	private MongoTemplate mongoTemplate;
 	
+	@Value("${spring.data.mongodb.collection.bundle}")
+	private String bundleCollectionName;
+	
 	public ResourceBundleServiceImpl(ResourceBundleRepository repository, MongoTemplate mongoTemplate) {
 		this.repository = repository;
 		this.mongoTemplate=mongoTemplate;
@@ -26,19 +31,21 @@ public class ResourceBundleServiceImpl implements ResourceBundleService{
 
 	@Override
 	public String createResourceBundle(ResourceBundle config) {
-		Document doc = new Document("_id",config.getKey())
-				.append("value", config.getValue());
-		mongoTemplate.getCollection("resource_bundle").insertOne(doc);
+		Document doc = new Document(ResourceBundleConstants.ID,config.getKey())
+				.append(ResourceBundleConstants.VALUE, config.getValue())
+				.append(ResourceBundleConstants.SITE_VALUES, config.getSiteValues());
+		mongoTemplate.getCollection(bundleCollectionName).insertOne(doc);
 		return "Values configured successfully " +config.getKey();
 	}
 
 	@Override
 	public String updateResourceBundle(ResourceBundle config) {
-		Document doc = new Document("_id",config.getKey())
-				.append("value", config.getValue());
-		Document filter = new Document("_id",config.getKey());
+		Document doc = new Document(ResourceBundleConstants.ID,config.getKey())
+				.append(ResourceBundleConstants.VALUE, config.getValue())
+				.append(ResourceBundleConstants.SITE_VALUES, config.getSiteValues());
+		Document filter = new Document(ResourceBundleConstants.ID,config.getKey());
 		ReplaceOptions options = new ReplaceOptions().upsert(true);
-		mongoTemplate.getCollection("resource_bundle").replaceOne(filter, doc, options);
+		mongoTemplate.getCollection(bundleCollectionName).replaceOne(filter, doc, options);
 		return "Values updated successfully " +config.getKey();
 	}
 	
@@ -54,8 +61,8 @@ public class ResourceBundleServiceImpl implements ResourceBundleService{
 
 	@Override
 	public String deleteResourceBundle(String key) {
-		Document filter = new Document("_id",key);
-		mongoTemplate.getCollection("resource_bundle").deleteOne(filter);
+		Document filter = new Document(ResourceBundleConstants.ID,key);
+		mongoTemplate.getCollection(bundleCollectionName).deleteOne(filter);
 		return "Resource bundle has been deleted " +key;
 	}
 
