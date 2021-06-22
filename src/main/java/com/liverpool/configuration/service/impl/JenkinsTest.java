b0934a184b4f139fcbfcb6dbda748906087a685b
abhishek-digisprint <83590864+abhishek-digisprint@users.noreply.github.com>
2021-06-22 15:44:51 +0530

    jenkins 4

--- src/main/java/com/liverpool/configuration/service/impl/JenkinsTest.java
+++ src/main/java/com/liverpool/configuration/service/impl/JenkinsTest.java
@@ -3,6 +3,6 @@ package src.main.java.com.liverpool.configuration.service.impl;
 public class JenkinsTest {
 	
 	public void run() {
-		System.out.println("jenkins trigger 3");
+		System.out.println("jenkins trigger 4");
 	}
 }
