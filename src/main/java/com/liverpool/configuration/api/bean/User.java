757dfb4311a1a07128238b217dbbaae36bff97ba
Rahul <rahulchowdary904@gmail.com>
2021-06-22 10:33:13 +0530

    initial commit
    login services

---
+++ src/main/java/com/liverpool/configuration/api/bean/User.java
@@ -0,0 +1,83 @@
+package com.liverpool.configuration.api.bean;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+public class User {
+	@Id
+	private String id;
+	@NotNull
+	private String userName;
+	@NotNull
+	private String password;
+	@NotNull
+	private String firstName;
+	@NotNull
+	private String lastName;
+	@NotNull
+	private String email;
+	/*@NotNull
+	private List<String> roles;*/
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/*public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}*/
+	
+	
+}
---
+++ src/test/java/com/liverpool/configuration/mango/api/LiverpoolConfigurationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.liverpool.configuration.mango.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class LiverpoolConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
