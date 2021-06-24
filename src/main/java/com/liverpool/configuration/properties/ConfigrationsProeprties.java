b369b4d6e552c5d6591a4503d230ff6112913949
ramesh970044 <71554695+ramesh970044@users.noreply.github.com>
2021-06-24 16:55:38 +0530

    Update ConfigrationsProeprties.java

--- src/main/java/com/liverpool/configuration/properties/ConfigrationsProeprties.java
+++ src/main/java/com/liverpool/configuration/properties/ConfigrationsProeprties.java
@@ -1,12 +1,14 @@
 package com.liverpool.configuration.properties;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Component
 @Data
+@Configuration
 public class ConfigrationsProeprties {
 	
 	@Value("${liverpool.configuration.statickeys.type}")
