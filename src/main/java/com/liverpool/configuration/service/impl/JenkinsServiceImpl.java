dfdd8c424bc223506be40e0e36015162509d926d
abhishek-digisprint <83590864+abhishek-digisprint@users.noreply.github.com>
2021-06-24 13:27:09 +0530

    jenkis trigger 11

--- src/main/java/com/liverpool/configuration/service/impl/JenkinsServiceImpl.java
+++ src/main/java/com/liverpool/configuration/service/impl/JenkinsServiceImpl.java
@@ -3,6 +3,6 @@ package com.liverpool.configuration.service.impl;
 public class JenkinsServiceImpl {
 
 	public void run() {
-		System.out.println("jenkis trigger 10");
+		System.out.println("jenkis trigger 11");
 	}
 }
