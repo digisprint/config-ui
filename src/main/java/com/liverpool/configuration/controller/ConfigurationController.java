2f33fc10dfcadabb7f7bb3c2ac2b561af16df356
spogulakonda <spogulakonda@digisprint.com>
2021-07-06 15:47:51 +0530

    Role services added without token
    CrossOrigin added

--- src/main/java/com/liverpool/configuration/controller/ConfigurationController.java
+++ src/main/java/com/liverpool/configuration/controller/ConfigurationController.java
@@ -1,9 +1,8 @@
 package com.liverpool.configuration.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@CrossOrigin(origins = "http://localhost:3000")
 public class ConfigurationController extends BaseController {
 
 	private RequestRedirectService redirectService;
