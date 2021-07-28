c220c8a43951b255aceb3fa085b29f2bb12fee3e
spogulakonda <85293268+spogulakonda@users.noreply.github.com>
2021-07-28 13:12:01 +0530

    Update LiverpoolConfigurationApplication.java

--- src/main/java/com/liverpool/configuration/LiverpoolConfigurationApplication.java
+++ src/main/java/com/liverpool/configuration/LiverpoolConfigurationApplication.java
@@ -3,10 +3,8 @@ package com.liverpool.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class LiverpoolConfigurationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
