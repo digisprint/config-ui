2566a6ff6d01f1cd3c86c6f274a2354398fbc6fd
gvemula <gopivemula1@gmail.com>
2021-08-20 23:13:13 +0530

    CORS resolved

--- src/main/java/com/liverpool/configuration/ConfigurationApplication.java
+++ src/main/java/com/liverpool/configuration/ConfigurationApplication.java
@@ -3,11 +3,27 @@ package com.liverpool.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ConfigurationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+				//.allowedOrigins("*" )
+				.allowCredentials(true);
+			}
+		};
 	}
 }
