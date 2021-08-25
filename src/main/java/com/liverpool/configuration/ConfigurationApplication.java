647fcd7cb7cb4c984d410627dd7f9c61ef9d90b0
spogulakonda <spogulakonda@digisprint.com>
2021-08-25 17:28:36 +0530

    Removed spring cloud config server

--- src/main/java/com/liverpool/configuration/ConfigurationApplication.java
+++ src/main/java/com/liverpool/configuration/ConfigurationApplication.java
@@ -2,14 +2,12 @@ package com.liverpool.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class ConfigurationApplication  {
 
 	public static void main(String[] args) {
